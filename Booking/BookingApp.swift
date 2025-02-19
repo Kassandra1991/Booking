9449b4cea4dd10684e3460f7ea8bda0ba2966e83
Alex Asichka <sanechka3@mail.ru>
2025-02-19 10:41:43 +0200

    Initial Commit

---
+++ Booking/BookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingApp.swift
+//  Booking
+//
+//  Created by Aleksandra Asichka on 19/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
